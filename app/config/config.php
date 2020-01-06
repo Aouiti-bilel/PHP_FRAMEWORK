4ed6782b8557db234465bb1880fb32c47204eef2
Aouiti-bilel <khelifa.bilel1@outlook.fr>
2020-01-06 04:42:53 +0100

    Config File & Uploader

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+//App route
+define('APPROOT', dirname(dirname(__File__)));
+// URL Root
+define('URLROOT', 'http://localhost/PHP_FRAMEWORK');
+
+//site name
+
+define('SITENAME', 'App Name ');
