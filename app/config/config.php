d3c7a18212a60b078c67ac962d202d2d7bfc3007
Aouiti-bilel <khelifa.bilel1@outlook.fr>
2020-01-07 12:01:28 +0100

    Pages_ Bootstrap & Navbar

--- app/config/config.php
+++ app/config/config.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', '');
+define('DB_NAME', 'Shareposts');
 
 //App route
 define('APPROOT', dirname(dirname(__File__)));
@@ -13,4 +13,6 @@ define('URLROOT', 'http://localhost/PHP_FRAMEWORK');
 
 //site name
 
-define('SITENAME', 'App Name ');
+define('SITENAME', 'Share Posts');
+  // App Version
+  define('APPVERSION', '1.0.0');
